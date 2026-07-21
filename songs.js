1f0a2855d667be89662303612d88ce57da64fc48
Nekoz <triclone1234@gmail.com>
2026-07-21 13:50:16 +0700

    Update songs.js

--- songs.js
+++ songs.js
@@ -3,8 +3,18 @@
 
 const songs = [
   {
-    title: "Không Bằng",
+    title: "Cự Tuyệt x Tìm Em",
+    artist: "ChillLome Remix",
+    src: "song/Cự Tuyệt x Tìm Em  ChillLome Remix.mp3"
+  },
+  {
+    title: "Không Buông",
     artist: "Hngle ft. Ari",
-    src: "song/Hngle - KHÔNG BẰNG ft. Ari  Official Music Video.mp3"
+    src: "song/Hngle - KHÔNG BUÔNG ft. Ari  Official Music Video.mp3"
+  },
+  {
+    title: "Simp Gái 808",
+    artist: "Low G",
+    src: "song/Simp Gái 808  Low G  Rap Nhà Làm.mp3"
   }
 ];
