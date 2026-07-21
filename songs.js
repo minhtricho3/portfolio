35d330bfb279430f2f00f91ad597c275635d55a5
Nekoz <triclone1234@gmail.com>
2026-07-21 13:47:29 +0700

    new song

--- songs.js
+++ songs.js
@@ -3,18 +3,8 @@
 
 const songs = [
   {
-    title: "Tên bài 1",
-    artist: "Nghệ sĩ 1",
-    src: "song/bai1.mp3"
-  },
-  {
-    title: "Tên bài 2",
-    artist: "Nghệ sĩ 2",
-    src: "song/bai2.mp3"
-  },
-  {
-    title: "Tên bài 3",
-    artist: "Nghệ sĩ 3",
-    src: "song/bai3.mp3"
+    title: "Không Bằng",
+    artist: "Hngle ft. Ari",
+    src: "song/Hngle - KHÔNG BẰNG ft. Ari  Official Music Video.mp3"
   }
 ];
